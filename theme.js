6f9feeeddd6a0879aa60cdfc58e98a64d8e19ad3
usny003-netizen <usny003@gmail.com>
2026-07-21 11:48:46 +0700

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,1 +1,101 @@
+/* =================================
+   Dinner Roulette ❤️
+   Theme System V2 🌙
+================================= */
 
+
+const themeBtn = document.getElementById("themeBtn");
+
+
+
+// โหลด Theme
+
+let darkMode =
+
+localStorage.getItem("darkMode") === "true";
+
+
+
+
+
+function applyTheme(){
+
+
+    if(darkMode){
+
+
+        document.body.classList.add(
+            "dark"
+        );
+
+
+        themeBtn.innerHTML =
+        "☀️ Light Mode";
+
+
+    }
+
+    else{
+
+
+        document.body.classList.remove(
+            "dark"
+        );
+
+
+        themeBtn.innerHTML =
+        "🌙 Dark Mode";
+
+
+    }
+
+
+
+}
+
+
+
+applyTheme();
+
+
+
+
+
+
+
+// ปุ่มเปลี่ยน Theme
+
+
+if(themeBtn){
+
+
+themeBtn.addEventListener(
+
+"click",
+
+()=>{
+
+
+    darkMode = !darkMode;
+
+
+
+    localStorage.setItem(
+
+        "darkMode",
+
+        darkMode
+
+    );
+
+
+
+    applyTheme();
+
+
+
+});
+
+
+
+}

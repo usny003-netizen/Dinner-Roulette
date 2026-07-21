db18bebf2b94d727bcc1c09bb6db13a0cef17b89
usny003-netizen <usny003@gmail.com>
2026-07-21 11:45:28 +0700

    Update favorite.js

--- favorite.js
+++ favorite.js
@@ -1,1 +1,169 @@
+/* =================================
+   Dinner Roulette ❤️
+   Favorite System ⭐
+================================= */
 
+
+const favoriteBtn = 
+document.getElementById("favoriteBtn");
+
+
+const favoriteList = 
+document.getElementById("favoriteList");
+
+
+
+
+// โหลดข้อมูล
+
+let favorites = JSON.parse(
+
+localStorage.getItem("favorites")
+
+) || [];
+
+
+
+
+// แสดงรายการ
+
+function showFavorites(){
+
+
+    if(!favoriteList)
+    return;
+
+
+
+    favoriteList.innerHTML="";
+
+
+
+    if(favorites.length === 0){
+
+
+        favoriteList.innerHTML =
+
+        "<li>ยังไม่มีเมนูโปรด</li>";
+
+        return;
+
+    }
+
+
+
+    favorites.forEach(food=>{
+
+
+        const li = document.createElement("li");
+
+
+        li.innerHTML =
+
+        "⭐ " + food;
+
+
+
+        favoriteList.appendChild(li);
+
+
+    });
+
+
+
+}
+
+
+
+showFavorites();
+
+
+
+
+
+
+
+// บันทึกเมนู
+
+
+if(favoriteBtn){
+
+
+favoriteBtn.addEventListener(
+
+"click",
+
+()=>{
+
+
+    const food =
+
+    window.currentFood;
+
+
+
+    if(!food){
+
+
+        alert(
+
+        "กรุณาสุ่มอาหารก่อน ❤️"
+
+        );
+
+        return;
+
+    }
+
+
+
+
+
+    if(!favorites.includes(food)){
+
+
+        favorites.push(food);
+
+
+
+        localStorage.setItem(
+
+        "favorites",
+
+        JSON.stringify(favorites)
+
+        );
+
+
+
+        showFavorites();
+
+
+
+        alert(
+
+        "⭐ เพิ่มเมนูโปรดแล้ว"
+
+        );
+
+
+    }
+
+    else{
+
+
+        alert(
+
+        "เมนูนี้มีในรายการแล้ว"
+
+        );
+
+
+    }
+
+
+
+});
+
+
+}
